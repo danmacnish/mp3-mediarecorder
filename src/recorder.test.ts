8fdc900bc11eaebb456c40e34e1bfe0e4ff46344
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-28 00:59:38 +0000

    Deploy eliasmeire/mp3-mediarecorder to github.com/eliasmeire/mp3-mediarecorder.git:gh-pages

--- src/recorder.test.ts
+++
@@ -1,7 +0,0 @@
-import { getMp3MediaRecorder } from './recorder';
-
-describe('mp3-mediarecorder', () => {
-    it('should have an export called getMp3MediaRecorder', () => {
-        expect(getMp3MediaRecorder).toBeDefined();
-    });
-});
